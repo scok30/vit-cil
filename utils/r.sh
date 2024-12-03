7a90f060d64009e483ebd98019f7d3c8ac6ffccd
翟江天 <1271262341@qq.com>
2024-12-03 12:13:27 +0800

    update config

---
+++ utils/r.sh
@@ -0,0 +1,2 @@
+python main.py --config=./exps/fcs/cifar100/5/first_stage.json 
+python main.py --config=./exps/fcs/cifar100/5/second_stage.json 
---
+++ utils/trainer.py
@@ -0,0 +1,106 @@
+import sys
+import logging
+import copy
+import torch
+from utils import factory
+from utils.data_manager import DataManager
+import os
+import pdb
+import json
+
+def train(args):
+    seed_list = copy.deepcopy(args["seed"])
+    device = copy.deepcopy(args["device"])
+    for seed in seed_list:
+        args["seed"] = seed
+        args["device"] = device
+        _train(args)
+
+
+def _train(args):
+
+    init_cls = 0 if args ["init_cls"] == args["increment"] else args["init_cls"]
+    log_dir = args["log_dir"]
+    logs_name = "{}/{}/{}/{}/{}".format(args["model_name"],args["dataset"], init_cls, args['increment'],args['log_name'])
+    logs_name = os.path.join(log_dir,logs_name)
+    if not os.path.exists(logs_name):
+        os.makedirs(logs_name)
+
+    logfilename =os.path.join(log_dir,"{}/{}/{}/{}/{}/{}_{}_{}".format(
+        args["model_name"],
+        args["dataset"],
+        init_cls,
+        args["increment"],
+        args['log_name'],
+        args["prefix"],
+        args["seed"],
+        args["convnet_type"],
+    ))
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(filename)s] => %(message)s",
+        handlers=[
+            logging.FileHandler(filename=logfilename + ".log"),
+            logging.StreamHandler(sys.stdout),
+        ],
+    )
+    _set_random()
+    _set_device(args)
+    print_args(args)
+    data_manager = DataManager(
+        args["dataset"],
+        args["shuffle"],
+        args["seed"],
+        args["init_cls"],
+        args["increment"],
+    )
+    model = factory.get_model(args["model_name"], args)
+
+    cnn_curve = {"top1": [], "top5": []}
+
+    for task in range(data_manager.nb_tasks):
+        model.incremental_train(data_manager)
+        cnn_accy, nme_accy = model.eval_task()
+
+        logging.info("CNN: {}".format(cnn_accy["grouped"]))
+        logging.info("NME: {}".format(nme_accy["grouped"]))
+
+        cnn_curve["top1"].append(cnn_accy["top1"])
+        cnn_curve["top5"].append(cnn_accy["top5"])
+
+        logging.info("CNN top1 curve: {}".format(cnn_curve["top1"]))
+        logging.info("CNN top5 curve: {}".format(cnn_curve["top5"]))
+        
+        cnn_accy, nme_accy = model.eval_task(only_new=True)
+        cnn_accy, nme_accy = model.eval_task(only_old=True)
+
+        
+        model.after_task()
+        if args["is_task0"] :
+            break 
+def _set_device(args):
+    device_type = args["device"]
+    gpus = []
+
+    for device in device_type:
+        if device_type == -1:
+            device = torch.device("cpu")
+        else:
+            device = torch.device("cuda:{}".format(device))
+
+        gpus.append(device)
+
+    args["device"] = gpus
+
+
+def _set_random():
+    torch.manual_seed(1)
+    torch.cuda.manual_seed(1)
+    torch.cuda.manual_seed_all(1)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def print_args(args):
+    for key, value in args.items():
+        logging.info("{}: {}".format(key, value))
